92c2c19ae2ec2bfe92d605ff0390982eb19ce30f
MichalMiskiewicz <miskiewiczmd@gmail.com>
2021-04-24 19:21:13 +0200

    Add routing php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo "Hello World!";
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('index', 'DefaultController');
+Routing::get('products', 'DefaultController');
+Routing::get('favourites', 'DefaultController');
+Routing::get('addproduct', 'DefaultController');
+Routing::run($path);
---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'src/controllers/AppController.php';
+
+class DefaultController extends AppController {
+    
+    public function index() {
+        $this->render('login');
+    }
+
+    public function products() {
+        $this->render('products');
+    }
+
+    public function favourites() {
+        $this->render('favourites');
+    }
+
+    public function addproduct() {
+        $this->render('addproduct');
+    }
+}
